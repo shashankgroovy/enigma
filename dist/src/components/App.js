7c9ab0bd52417f218f211516ffb66b0cd403eb14
Shashank Srivastav <shashankgroovy@gmail.com>
2019-08-05 00:58:50 +0530

    Adds footer

--- dist/src/components/App.js
+++ dist/src/components/App.js
@@ -4,15 +4,15 @@ export default {
     <div id="app" class="container">
 
       <div class="row">
-        <router-link v-bind:to="'/'">
-          <div class="one-half column jutsu-box ">
-            <p class="jutsu">ENIGMA</p>
-            <h3>Share a secret with the universe!</h3>
-          </div>
+          <router-link v-bind:to="'/'">
+            <div class="one-half column jutsu-box ">
+              <p class="jutsu">ENIGMA</p>
+              <h3>Share a secret with the universe!</h3>
+            </div>
+          </router-link>
           <div class="one-half column image-box">
             <img class="sealing-jutsu" src="/dist/static/images/logo.png" alt="Eight Trigrams Sealing Jutsu">
           </div>
-        </router-link>
       </div>
 
       <div class="row">
